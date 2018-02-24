3ceb44b5c2ea9f02b2c64408dde056166e75cc21
RomirPatney <romir.patney@gmail.com>
2018-02-24 01:54:04 -0500

    API part

---
+++ API_Transnet/API_Transnet/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace API_Transnet.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ API_Transnet/API_Transnet/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_Transnet.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
