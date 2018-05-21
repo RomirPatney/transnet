5333af746e5cb9b5f2fed96699c988f4d3162c80
RomirPatney <romir.patney@gmail.com>
2018-05-21 17:28:55 -0400

    removed passwords and subscription from azure

--- API_transnet_1.1/API_transnet_1.1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace API_transnet_1._1.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- API_transnet_1.1/API_transnet_1.1/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace API_transnet_1._1.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
